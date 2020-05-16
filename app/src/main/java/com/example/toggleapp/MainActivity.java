9ac31c65a6a12bdc5494c5621a2f59de77e59136
Chia wan Chang <ss105213025@gmail.com>
2020-05-16 13:19:52 +0800

    debug-1

--- app/src/main/java/com/example/toggleapp/MainActivity.java
+++ app/src/main/java/com/example/toggleapp/MainActivity.java
@@ -25,24 +25,23 @@ public class MainActivity extends AppCompatActivity implements CompoundButton.On
         img2 = (ImageView) findViewById(R.id.img2);
 
         tog_btn = (ToggleButton) findViewById(R.id.tog_btn);
+        tog_btn.setOnCheckedChangeListener(this);
 
         constraintLayout = (ConstraintLayout) findViewById(R.id.constraintLayout);
-
-        constraintLayout.setBackgroundColor(Color.parseColor("FFD1BA75"));
-
-        tog_btn.setOnCheckedChangeListener(this);
+        constraintLayout.setBackgroundColor(Color.parseColor("#000000"));
     }
 
     @Override
     public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
         if (isChecked) {
-            constraintLayout.setBackgroundColor(Color.parseColor("FF0E0E0E"));
-            img1.setVisibility(View.VISIBLE);
-            img2.setVisibility(View.INVISIBLE);
-        } else {
-            constraintLayout.setBackgroundColor(Color.parseColor("FFD1BA75"));
+            //ParseColor 只能使用RGB色碼，不可用HEX
+            constraintLayout.setBackgroundColor(Color.parseColor("#FFFF99"));
             img1.setVisibility(View.INVISIBLE);
             img2.setVisibility(View.VISIBLE);
+        } else {
+            constraintLayout.setBackgroundColor(Color.parseColor("#000000"));
+            img1.setVisibility(View.VISIBLE);
+            img2.setVisibility(View.INVISIBLE);
         }
     }
 }
